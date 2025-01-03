75b401e9d9830478cf865adedc5d581dd0fbf820
serat <serdarovatajan55@gmail.com>
2025-01-03 03:52:03 +0300

    Update api.py
    Rewritten for asynchrony

--- fastapi/api.py
+++ fastapi/api.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from sqlalchemy import create_engine, text
-from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import MetaData, text
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import select, delete
 
 tags = ['Задачи 📃']
-engine = create_engine('postgresql://postgres:serat@localhost:5432/tasks', echo=False)
-new_session = sessionmaker(engine)
+engine = create_async_engine('postgresql+asyncpg://postgres:serat@localhost:5432/tasks', echo=False)
+new_session = async_sessionmaker(engine)
 app = FastAPI()
 metadata = MetaData()
 
@@ -38,54 +39,71 @@ class TaskORM(Base):
     completed: Mapped[bool]
     categories: Mapped[str | None]
 
-TaskORM.metadata.create_all(engine)
+async def init_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 @app.post('/create_task', summary='Создать задачу', tags=tags)
-def create_task(task: TaskSchema):
+async def create_task(task: TaskSchema):
+    task.categories = str(task.categories)
     task = TaskORM(title=task.title, body=task.body, completed=task.completed, categories=task.categories)
-    with new_session() as session:
+    async with new_session() as session:
         session.add(task)
-        session.commit()
+        await session.commit()
     return {'success': 'Успешно добавлено в базу'}
 
 @app.get('/get_tasks', summary='Получить список задач', tags=tags)
-def get_tasks_list():
-    with new_session() as session:
-        result = session.query(TaskORM).all()
+async def get_tasks_list():
+    async with new_session() as session:
+        query = select(TaskORM)
+        results = await session.execute(query)
+        result = results.scalars().all()
     return {'tasks': result}
 
 @app.get('/get_task/{id}', summary='Получить задачу по id', tags=tags)
-def get_task(id: int):
-    with new_session() as session:
-        result = session.query(TaskORM).filter_by(id=id).all()
+async def get_task(id: int):
+    async with new_session() as session:
+        query = select(TaskORM).filter_by(id=id)
+        results = await session.execute(query)
+        result = results.scalars().all()
     return {'task': result}
 
 @app.delete('/delete_task/{id}', summary='Удалить задачу', tags=tags)
-def delete_task(id: int):
-    with new_session() as session:
-        ids = session.query(TaskORM.id).all()
-        if id in ids[0]:
-            session.query(TaskORM).filter_by(id=id).delete()
-            session.commit()
+async def delete_task(id: int):
+    async with new_session() as session:
+        query = select(TaskORM.id)
+        ids = await session.execute(query)
+        ids = ids.scalars().all()
+        if id in ids:
+            query = select(TaskORM).filter_by(id=id)
+            result = await session.execute(query)
+            task = result.scalars().first()
+            await session.delete(task)
+            await session.commit()
             return {'success': 'Успешно удалено из базы'}
         else:
             return HTTPException(status_code=404, detail='ID нет в базе')
 
 @app.get('/get_tasks_by_categories/{category}', summary='Получить все задачи по категориям', tags=tags)
-def get_tasks_by_categories(category: str):
-    with new_session() as session:
-        sql = text("""
+async def get_tasks_by_categories(category: str):
+    async with new_session() as session:
+        query = text("""
         SELECT * FROM tasks
         WHERE categories LIKE :category;
         """)
-        result = session.execute(sql, {'category': f"%{category}%"}).mappings().all()
+        results = await session.execute(query, {'category': f"%{category}%"})
+        result = results.mappings().all()
+        print(result)
     return {'tasks': result}
 
 
 @app.post('/update_task_status/{id}', summary='Обновить статус задачи', tags=tags)
-def update_task_status(id: int):
-    with new_session() as session:
-        completed = session.query(TaskORM).filter_by(id=id).first()
-        completed.completed = True
-        session.commit()
+async def update_task_status(id: int):
+    async with new_session() as session:
+        query = select(TaskORM).filter_by(id=id)
+        results = await session.execute(query)
+        result = results.scalars().first()
+        result.completed = True
+        await session.commit()
     return {'success': 'Статус задачи обновлён'}
